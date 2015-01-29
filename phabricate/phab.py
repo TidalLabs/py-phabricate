9fec7c89956b145b612e9624238bbcba138999c6
Noemi Millman <noemi@tid.al>
2015-01-29 17:53:01 -0500

    utilities to fetch user object / user ID by alias

--- phabricate/phab.py
+++ phabricate/phab.py
@@ -1,4 +1,16 @@
 from phabricator import Phabricator as _Phabricator
 
 class Phabricator(_Phabricator):
-	pass
+	def get_user(self, alias):
+		"""
+		Utility to fetch a single user's data by alias
+		"""
+		return self.user.find(aliases=[alias,])
+
+	def get_user_id(self, alias):
+		"""
+		API returns user ID in an attribute named by the alias
+		Which is of course a giant PITA to access.
+		"""
+		user = self.get_user(alias)
+		return getattr(user, alias)
