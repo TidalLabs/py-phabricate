e7f272716127a9cbb97b6af0075b66ea4c2d4fb2
Noemi Millman <noemi@tid.al>
2015-01-29 18:04:47 -0500

    improve docstrings

--- phabricate/phab.py
+++ phabricate/phab.py
@@ -1,9 +1,18 @@
 from phabricator import Phabricator as _Phabricator
 
 class Phabricator(_Phabricator):
+	"""
+	Wrapper class for phabricator.Phabricator from python-phabricator.
+	Provides convenience methods for easier access to queries and attributes
+	that are awkward with python-phabricator due to Conduit quirks.
+	"""
+
 	def get_user(self, alias):
 		"""
-		Utility to fetch a single user's data by alias
+		Utility to fetch a single user's data by username
+
+		@param str alias	The username of the user you want to fetch
+		@return Result		python-phabricator Result object containing user data
 		"""
 		return self.user.find(aliases=[alias,])
 
@@ -11,6 +20,9 @@ class Phabricator(_Phabricator):
 		"""
 		API returns user ID in an attribute named by the alias
 		Which is of course a giant PITA to access.
+
+		@param str alias	The username of the user you want to fetch
+		@return str 		The Phabricator id ("phid") of the user in question
 		"""
 		user = self.get_user(alias)
 		return getattr(user, alias)
